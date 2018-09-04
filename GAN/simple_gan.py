765647df079cd06179b695ddefd37b3a931f336d
Sander Dalm (DBS) <sander@holmes.nl>
2018-09-04 14:49:39 +0200

    Simple conv GAN werkt.

--- GAN/simple_gan.py
+++ GAN/simple_gan.py
@@ -33,19 +33,13 @@ def UpConv2D(x, filters, kernel_size, stride, padding='same'):
 def sample_Z(batch_size, dim=[25, 25, 32]):
     return np.random.uniform(-1., 1., size=[batch_size]+dim)
 
-def sample_data(batch_size):
-    x, y = bg.generate_train_batch(batch_size)
+def generator(noise, reuse=False):
+    with tf.variable_scope("GAN/Generator", reuse=reuse):
+        x = UpConv2D(x=noise, filters=16, kernel_size=3, stride=1)
+        x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
+        x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
+        x = UpConv2D(x=x, filters=3, kernel_size=3, stride=2)
     return x
-
-
-def generator(Z, reuse=False):
-        with tf.variable_scope("GAN/Generator", reuse=reuse):
-            x = UpConv2D(x=Z, filters=16, kernel_size=3, stride=1)
-            x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
-            x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
-            x = UpConv2D(x=x, filters=3, kernel_size=3, stride=2)
-        return x
-
 
 
 def discriminator(img, reuse=False):
@@ -74,43 +68,22 @@ def discriminator(img, reuse=False):
 
         x = tf.layers.flatten(x)
 
-    return x
+        x = tf.layers.dense(inputs=x, units=1, activation=None)
 
+    return x
 
 H = 200
 W = 200
-DIM = int(H/8)
+NOISE_DIM1, NOISE_DIM2, NOISE_DIM3 = 25, 25, 32
 bg = BatchGenerator(path=config.datadir, height=H, width=W, datasets=['utkf'])
 
+# Define graph
+real_img = tf.placeholder(tf.float32, [None, H, W, 3])
+noise = tf.placeholder(tf.float32, [None, NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])
 
-###############
-
-Zp = tf.placeholder(tf.float32, [None, DIM, DIM, 32])
-test = generator(Zp, False)
-test2 = discriminator(test)
-
-sess = tf.Session()
-tf.global_variables_initializer().run(session=sess)
-
-
-Z = sample_Z(1, [DIM, DIM, 32])
-Z.shape
-
-test, test2 = sess.run([test, test2], feed_dict={Zp: Z})
-
-test.shape
-test2.shape
-plt.imshow(test[0])
-
-###############
-
-
-X = tf.placeholder(tf.float32, [None, H, W, 3])
-Z = tf.placeholder(tf.float32, [None, DIM])
-
-G_sample = generator(Z)
-r_logits, r_rep = discriminator(X)
-f_logits, g_rep = discriminator(G_sample, reuse=True)
+fake_img = generator(noise)
+r_logits = discriminator(real_img)
+f_logits = discriminator(fake_img, reuse=True)
 
 disc_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=r_logits, labels=tf.ones_like(
     r_logits)) + tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits, labels=tf.zeros_like(f_logits)))
@@ -119,28 +92,40 @@ gen_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logit
 gen_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="GAN/Generator")
 disc_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="GAN/Discriminator")
 
-gen_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(gen_loss, var_list=gen_vars)  # G Train step
-disc_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(disc_loss, var_list=disc_vars)  # D Train step
+gen_step = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=.5, epsilon=.1).minimize(gen_loss, var_list=gen_vars)  # G Train step
+disc_step = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=.5, epsilon=.1).minimize(disc_loss, var_list=disc_vars)  # D Train step
 
 # sess = tf.Session(config=config)
 sess = tf.Session()
 tf.global_variables_initializer().run(session=sess)
 
-batch_size = 4
+batch_size = 32
 nd_steps = 10
 ng_steps = 10
 
-for i in range(10001):
-    X_batch = sample_data(batch_size, 2)
-    Z_batch = sample_Z(batch_size, 32, 32)
+d_losses = []
+g_losses = []
+
+for i in range(100):
+    print(i)
 
     for _ in range(nd_steps):
-        _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
-    rrep_dstep, grep_dstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
+        img_batch, _ = bg.generate_train_batch(batch_size)
+        noise_batch = sample_Z(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])
+        _, dloss = sess.run([disc_step, disc_loss], feed_dict={real_img: img_batch, noise: noise_batch})
 
     for _ in range(ng_steps):
-        _, gloss = sess.run([gen_step, gen_loss], feed_dict={Z: Z_batch})
+        img_batch, _ = bg.generate_train_batch(batch_size)
+        noise_batch = sample_Z(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])
+        _, gloss = sess.run([gen_step, gen_loss], feed_dict={noise: noise_batch})
 
-    rrep_gstep, grep_gstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
+    d_losses.append(dloss)
+    g_losses.append(gloss)
 
-print(sess.run([G_sample], feed_dict={Z: sample_Z(1, 2)}))
+plt.plot(d_losses, alpha =.8, color='g')
+plt.plot(g_losses, alpha =.8, color='b')
+
+generated_image =  sess.run([fake_img], feed_dict={noise: sample_Z(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])})
+img = generated_image[0][0]
+img
+plt.imshow(img)
