b6ed95c4afe618ac9cbc3775274c779edd84b8c6
Sander Dalm (DBS) <sander@holmes.nl>
2018-09-04 14:12:19 +0200

    Conv erin. Labels moeten nog.

--- GAN/simple_gan.py
+++ GAN/simple_gan.py
@@ -6,34 +6,104 @@ import matplotlib.pyplot as plt
 from age_regressor.batch_generator import BatchGenerator
 import config
 
-def sample_Z(batch_size, n):
-    return np.random.uniform(-1., 1., size=[batch_size, n])
+
+def Conv2D(x, filters, kernel_size, stride, padding='same'):
+    return tf.layers.conv2d(inputs=x,
+                            filters=filters,
+                            kernel_size=kernel_size,
+                            strides=stride,
+                            activation=tf.nn.relu,
+                            kernel_initializer=tf.keras.initializers.he_normal(),
+                            kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
+                            activity_regularizer=tf.keras.regularizers.l2(l=0.01),
+                            padding=padding)
+
+
+def UpConv2D(x, filters, kernel_size, stride, padding='same'):
+    return tf.layers.conv2d_transpose(inputs=x,
+                                      filters=filters,
+                                      kernel_size=kernel_size,
+                                      strides=stride,
+                                      activation=tf.nn.relu,
+                                      kernel_initializer=tf.keras.initializers.he_normal(),
+                                      kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
+                                      activity_regularizer=tf.keras.regularizers.l2(l=0.01),
+                                      padding=padding)
+
+def sample_Z(batch_size, dim=[25, 25, 32]):
+    return np.random.uniform(-1., 1., size=[batch_size]+dim)
 
 def sample_data(batch_size):
     x, y = bg.generate_train_batch(batch_size)
     return x
 
 
-def generator(Z, hsize=[16, 16], reuse=False):
-    with tf.variable_scope("GAN/Generator", reuse=reuse):
-        h1 = tf.layers.dense(Z, hsize[0], activation=tf.nn.leaky_relu)
-        h2 = tf.layers.dense(h1, hsize[1], activation=tf.nn.leaky_relu)
-        out = tf.layers.dense(h2, 2)
-    return out
+def generator(Z, reuse=False):
+        with tf.variable_scope("GAN/Generator", reuse=reuse):
+            x = UpConv2D(x=Z, filters=16, kernel_size=3, stride=1)
+            x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
+            x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
+            x = UpConv2D(x=x, filters=3, kernel_size=3, stride=2)
+        return x
 
-def discriminator(X, hsize=[16, 16], reuse=False):
+
+
+def discriminator(img, reuse=False):
     with tf.variable_scope("GAN/Discriminator", reuse=reuse):
-        h1 = tf.layers.dense(X, hsize[0], activation=tf.nn.leaky_relu)
-        h2 = tf.layers.dense(h1, hsize[1], activation=tf.nn.leaky_relu)
-        h3 = tf.layers.dense(h2, 2)
-        out = tf.layers.dense(h3, 1)
-    return out, h3
+
+        filter_size = 4
+        x = Conv2D(img, filters=filter_size, kernel_size=3, stride=1)
+        x = Conv2D(x, filters=filter_size, kernel_size=3, stride=1)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+
+        x = Conv2D(x, filters=filter_size*2, kernel_size=3, stride=1)
+        x = Conv2D(x, filters=filter_size*2, kernel_size=3, stride=1)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+
+        x = Conv2D(x, filters=filter_size*4, kernel_size=3, stride=1)
+        x = Conv2D(x, filters=filter_size*4, kernel_size=3, stride=1)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+
+        x = Conv2D(x, filters=filter_size*8, kernel_size=3, stride=1)
+        x = Conv2D(x, filters=filter_size*8, kernel_size=3, stride=1)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+
+        x = Conv2D(x, filters=filter_size*16, kernel_size=3, stride=1)
+        x = Conv2D(x, filters=filter_size*16, kernel_size=3, stride=1)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+
+        x = tf.layers.flatten(x)
+
+    return x
 
 
 H = 200
 W = 200
-DIM = 256
+DIM = int(H/8)
 bg = BatchGenerator(path=config.datadir, height=H, width=W, datasets=['utkf'])
+
+
+###############
+
+Zp = tf.placeholder(tf.float32, [None, DIM, DIM, 32])
+test = generator(Zp, False)
+test2 = discriminator(test)
+
+sess = tf.Session()
+tf.global_variables_initializer().run(session=sess)
+
+
+Z = sample_Z(1, [DIM, DIM, 32])
+Z.shape
+
+test, test2 = sess.run([test, test2], feed_dict={Zp: Z})
+
+test.shape
+test2.shape
+plt.imshow(test[0])
+
+###############
+
 
 X = tf.placeholder(tf.float32, [None, H, W, 3])
 Z = tf.placeholder(tf.float32, [None, DIM])
@@ -62,7 +132,7 @@ ng_steps = 10
 
 for i in range(10001):
     X_batch = sample_data(batch_size, 2)
-    Z_batch = sample_Z(batch_size, 2)
+    Z_batch = sample_Z(batch_size, 32, 32)
 
     for _ in range(nd_steps):
         _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
