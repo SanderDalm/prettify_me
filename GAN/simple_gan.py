a7224359ad859e049ee37c0ce0f3ae274056386f
sander <sanderdalm@hotmail.com>
2018-09-05 13:05:18 +0200

    Working simple GAN.

--- GAN/simple_gan.py
+++ GAN/simple_gan.py
@@ -41,7 +41,7 @@ def generator(noise, reuse=False):
         x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
         x = UpConv2D(x=x, filters=16, kernel_size=3, stride=2)
         x = UpConv2D(x=x, filters=3, kernel_size=3, stride=2)
-    return tf.nn.sigmoid(x)
+    return tf.clip_by_value(x, 0, 1)
 
 
 def discriminator(img, reuse=False):
@@ -78,7 +78,7 @@ H = 32
 W = 32
 NOISE_DIM1, NOISE_DIM2, NOISE_DIM3 = int(H/8), int(W/8), 1
 bg = BatchGenerator(path=config.datadir, height=H, width=W, datasets=['utkf'])
-#x, y = bg.generate_train_batch(32)
+x, y = bg.generate_train_batch(32)
 #plt.imshow(x[0])
 
 # Define graph
@@ -99,37 +99,39 @@ disc_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="GAN/Discrimi
 gen_step = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=.5, epsilon=.1).minimize(gen_loss, var_list=gen_vars)  # G Train step
 disc_step = tf.train.AdamOptimizer(learning_rate=0.0001, beta1=.5, epsilon=.1).minimize(disc_loss, var_list=disc_vars)  # D Train step
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.Session(config=tf.ConfigProto())#log_device_placement=True
 tf.global_variables_initializer().run(session=sess)
 
 batch_size = 32
 nd_steps = 10
 ng_steps = 10
 
+
 d_losses = []
 g_losses = []
 
-for i in range(100):
+for i in range(1000):
     print(i)
 
     for _ in range(nd_steps):
         img_batch, _ = bg.generate_train_batch(batch_size)
         noise_batch = sample_noise(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])
         _, dloss = sess.run([disc_step, disc_loss], feed_dict={real_img: img_batch, noise: noise_batch})
-
-    for _ in range(ng_steps):
-        img_batch, _ = bg.generate_train_batch(batch_size)
-        noise_batch = sample_noise(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])
-        _, gloss = sess.run([gen_step, gen_loss], feed_dict={noise: noise_batch})
-
     d_losses.append(dloss)
-    g_losses.append(gloss)
+
+    if i > 200:
+        for _ in range(ng_steps):
+            img_batch, _ = bg.generate_train_batch(batch_size)
+            noise_batch = sample_noise(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])
+            _, gloss = sess.run([gen_step, gen_loss], feed_dict={noise: noise_batch})
+        g_losses.append(gloss)
+
 
 plt.plot(d_losses, alpha=.8, color='g')
 plt.plot(g_losses, alpha=.8, color='b')
 
 generated_image = sess.run([fake_img], feed_dict={noise: sample_noise(batch_size, [NOISE_DIM1, NOISE_DIM2, NOISE_DIM3])})
-img = generated_image[0][5]
+img = generated_image[0][0]
 img
 img.shape
 np.min(img)
