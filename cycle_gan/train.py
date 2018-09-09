dcad4f12c9ac24473b4eae70567963bc6d8a4a34
sander <sanderdalm@hotmail.com>
2018-09-09 21:17:41 +0200

    Opzetje cycle gan.

--- cycle_gan/train.py
+++ cycle_gan/train.py
@@ -11,7 +11,7 @@ import numpy as np
 import tensorflow as tf
 
 
-""" param """
+#""" param """
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--dataset', dest='dataset', default='horse2zebra', help='which dataset to use')
 parser.add_argument('--load_size', dest='load_size', type=int, default=286, help='scale images to this size')
@@ -29,47 +29,58 @@ batch_size = args.batch_size
 lr = args.lr
 
 
-""" graph """
+#""" graph """
 # models
 generator_a2b = partial(models.generator, scope='a2b')
 generator_b2a = partial(models.generator, scope='b2a')
 discriminator_a = partial(models.discriminator, scope='a')
 discriminator_b = partial(models.discriminator, scope='b')
 
-# operations
+# Placeholders
 a_real = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
 b_real = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
-a2b_sample = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
-b2a_sample = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
+# a2b_sample = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
+# b2a_sample = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
 
+# Generator outputs
 a2b = generator_a2b(a_real)
-b2a = generator_b2a(b_real)
-b2a2b = generator_a2b(b2a)
 a2b2a = generator_b2a(a2b)
 
+b2a = generator_b2a(b_real)
+b2a2b = generator_a2b(b2a)
+
+
+# Discriminator outputs
+# a2b
 a_logit = discriminator_a(a_real)
 b2a_logit = discriminator_a(b2a)
-b2a_sample_logit = discriminator_a(b2a_sample)
+
+# b2a
 b_logit = discriminator_b(b_real)
 a2b_logit = discriminator_b(a2b)
-a2b_sample_logit = discriminator_b(a2b_sample)
 
-# losses
+# Sample ??
+# b2a_sample_logit = discriminator_a(b2a_sample)
+# a2b_sample_logit = discriminator_b(a2b_sample)
+
+# Generator losses
 g_loss_a2b = tf.losses.mean_squared_error(a2b_logit, tf.ones_like(a2b_logit))
 g_loss_b2a = tf.losses.mean_squared_error(b2a_logit, tf.ones_like(b2a_logit))
 cyc_loss_a = tf.losses.absolute_difference(a_real, a2b2a)
 cyc_loss_b = tf.losses.absolute_difference(b_real, b2a2b)
 g_loss = g_loss_a2b + g_loss_b2a + cyc_loss_a * 10.0 + cyc_loss_b * 10.0
 
+# Discriminator a losses
 d_loss_a_real = tf.losses.mean_squared_error(a_logit, tf.ones_like(a_logit))
-d_loss_b2a_sample = tf.losses.mean_squared_error(b2a_sample_logit, tf.zeros_like(b2a_sample_logit))
-d_loss_a = d_loss_a_real + d_loss_b2a_sample
+d_loss_b2a = tf.losses.mean_squared_error(b2a_logit, tf.zeros_like(b2a_logit))
+d_loss_a = d_loss_a_real + d_loss_b2a
 
+# Discriminator b losses
 d_loss_b_real = tf.losses.mean_squared_error(b_logit, tf.ones_like(b_logit))
-d_loss_a2b_sample = tf.losses.mean_squared_error(a2b_sample_logit, tf.zeros_like(a2b_sample_logit))
-d_loss_b = d_loss_b_real + d_loss_a2b_sample
+d_loss_a2b = tf.losses.mean_squared_error(a2b_logit, tf.zeros_like(a2b_logit))
+d_loss_b = d_loss_b_real + d_loss_a2b
 
-# optim
+# Optimization
 t_var = tf.trainable_variables()
 d_a_var = [var for var in t_var if 'a_discriminator' in var.name]
 d_b_var = [var for var in t_var if 'b_discriminator' in var.name]
@@ -80,47 +91,41 @@ d_b_train_op = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(d_loss_b, var_list
 g_train_op = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(g_loss, var_list=g_var)
 
 
-""" train """
-''' init '''
+# """ train """
+# ''' init '''
 # session
 config = tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 
 
-''' saver '''
+#''' saver '''
 saver = tf.train.Saver(max_to_keep=5)
 
-'''train'''
+#'''train'''
 for i in range(1000):
-    a2b_opt, b2a_opt = sess.run([a2b, b2a], feed_dict={a_real: a_real_ipt, b_real: b_real_ipt})
 
-    # train G
-    g_summary_opt, _ = sess.run([g_summary, g_train_op], feed_dict={a_real: a_real_ipt, b_real: b_real_ipt})
-    summary_writer.add_summary(g_summary_opt, it)
-    # train D_b
-    d_summary_b_opt, _ = sess.run([d_summary_b, d_b_train_op], feed_dict={b_real: b_real_ipt, a2b_sample: a2b_sample_ipt})
-    summary_writer.add_summary(d_summary_b_opt, it)
-    # train D_a
-    d_summary_a_opt, _ = sess.run([d_summary_a, d_a_train_op], feed_dict={a_real: a_real_ipt, b2a_sample: b2a_sample_ipt})
-    summary_writer.add_summary(d_summary_a_opt, it)
+    batchgen = None
+    a_real_batch, b_real_batch = batchgen.generate_batch()
 
-    # display
-    if it % 1 == 0:
-        print("Epoch: (%3d) (%5d/%5d)" % (epoch, it_epoch, batch_epoch))
+    # train G a+b
+    g_summary_opt, _ = sess.run([g_train_op], feed_dict={a_real: a_real_batch, b_real: b_real_batch})
+
+    # train D a
+    d_summary_a_opt, _ = sess.run([d_a_train_op], feed_dict={a_real: a_real_batch})
+
+    # train D b
+    d_summary_b_opt, _ = sess.run([d_b_train_op], feed_dict={b_real: b_real_batch})
 
     # save
-    if (it + 1) % 1000 == 0:
-        save_path = saver.save(sess, '%s/Epoch_(%d)_(%dof%d).ckpt' % (ckpt_dir, epoch, it_epoch, batch_epoch))
+    if i + 1 % 1000 == 0:
+        save_path = saver.save(sess, 'path')
         print('Model saved in file: % s' % save_path)
 
     # sample
-    if (it + 1) % 100 == 0:
-        a_real_ipt = a_test_pool.batch()
-        b_real_ipt = b_test_pool.batch()
-        [a2b_opt, a2b2a_opt, b2a_opt, b2a2b_opt] = sess.run([a2b, a2b2a, b2a, b2a2b], feed_dict={a_real: a_real_ipt, b_real: b_real_ipt})
-        sample_opt = np.concatenate((a_real_ipt, a2b_opt, a2b2a_opt, b_real_ipt, b2a_opt, b2a2b_opt), axis=0)
+    if i + 1 % 100 == 0:
 
-        save_dir = './outputs/sample_images_while_training/' + dataset
-        utils.mkdir(save_dir)
-        im.imwrite(im.immerge(sample_opt, 2, 3), '%s/Epoch_(%d)_(%dof%d).jpg' % (save_dir, epoch, it_epoch, batch_epoch))
+        a2b_output, a2b2a_output, b2a_output, b2a2b_output = sess.run([a2b, a2b2a, b2a, b2a2b], feed_dict={a_real: a_real, b_real: b_real})
+        sample = np.concatenate((a2b_output, a2b2a_output, b2a_output, b2a2b_output), axis=0)
+        save_dir = '/outputs/'
+        # TO DO: save
